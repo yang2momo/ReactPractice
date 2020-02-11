6b22e9496470b614d27fbb5fa946feb1c20f3486
yang2momo <59380943+yang2momo@users.noreply.github.com>
2020-02-11 11:54:29 +0900

    vs 이론 공부

--- vs-study/public/index.js
+++ vs-study/public/index.js
@@ -1,3 +1,15 @@
-console.log('적힘?');
+const what = "공부를 합시당";
+console.log(what);
 
-alert('보이나?');
+const dog = {
+    name: "momo",
+    dog: "푸들"
+}
+
+const cat = {
+    name: "로로",
+    cat: "귀요미",
+    dog
+}
+
+console.log(cat.dog)
