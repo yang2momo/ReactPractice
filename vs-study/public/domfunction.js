79454b373d70e0a723f9de9729353ec59da340f3
yang2momo <59380943+yang2momo@users.noreply.github.com>
2020-02-12 19:09:59 +0900

    vs 공부

---
+++ vs-study/public/domfunction.js
@@ -0,0 +1,19 @@
+const title = document.getElementById("title");
+
+function handleClick() {
+    title.style.color = "red";
+}
+
+title.addEventListener("click", handleClick);
+// console.log(title);
+// // console.error("나가주그렴");
+
+// title.innerHTML = "가장 아름다운건 꾸준한 노력이다.";
+
+// function handleResize(event) {
+//     console.log(event);
+// }
+
+// // 폼을 만들거나 링크를 클릭할때 유용함.
+// window.addEventListener("resize", handleResize);
+
