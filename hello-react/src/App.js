1e03c524782586a5855238588f9ff095296267f3
yang2momo <59380943+yang2momo@users.noreply.github.com>
2020-02-12 10:57:58 +0900

    6장

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,22 +1,22 @@
 import React, { Component } from 'react';
-import ScrollBox from './ScrollBox';
+import IterationSample from './IterationSample';
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <ScrollBox ref={(ref) => this.ScrollBox = ref} />
-        <button onClick={() => this.ScrollBox.scrollToBottom()}>
-          맨 밑으로
-        </button>
-      </div>
-    );
-  }
-}
-// const App = () => {
-//   return <ValidationSample />;
-
-
+// class App extends Component {
+//   render() {
+//     return (
+//       <div>
+//         <ScrollBox ref={(ref) => this.ScrollBox = ref} />
+//         <button onClick={() => this.ScrollBox.scrollToBottom()}>
+//           맨 밑으로
+//         </button>
+//       </div>
+//     );
+//   }
 // }
+const App = () => {
+  return <IterationSample />;
+
+
+}
 
 export default App;
