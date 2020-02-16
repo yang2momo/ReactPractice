ca838a36d4e9b1b330b43a9ab95b61eb05897727
yang2momo <59380943+yang2momo@users.noreply.github.com>
2020-02-16 16:27:39 +0900

    vs 3.8강 까지
    후.. 어렵다.. 더공부행지..

--- vs-study/Momento 만들기.md
+++ vs-study/Momento 만들기.md
@@ -51,3 +51,57 @@
 - #### DIVIDE &CONQUER (쪼개서 공부해라!)
 
 - #### const(상수)
+
+- #### .querySelector
+
+  > HTML에서 필요한 것들을 얻음.
+
+- #### .length
+
+  > 를 쓰면 이 array 길이가 어느정도인지 알 수 있다. 
+
+- #### .push()
+
+  > 배열에 ()안에 있는거 집어넣기
+  >
+  > array 안에 element 하나를 넣어 줄 수있다.
+
+- local storage에는 , 자바스크립트의 data를 저장할 수 없다. 오직 string만 넣을 수 있다.
+
+- #### JSON.stringify
+
+  > 자바스크립트 object를 string으로 바꿔준다.
+
+- #### JSON
+
+  > JavaScript Object Notation
+  >
+  > 데이터를 전달할 때 , 자바스크립트가 그걸 다룰 수 있도록  object로 바꿔주는 기능이다.
+
+- #### JSON.parse(loadToDos)
+
+  > parse : string인것을 object로 바꿔줌
+
+- list에 있는 모든 item을 위한 함수를 실행시키는것... forEach, eilter
+
+- #### forEach
+
+  > array에 담겨있는 것들을 각각에 한번씩 함수를 실행시켜줌.
+  >
+  > 이 함수는 따로떼서 밖에다가 둘 수 있다.
+
+- delet child element mdn
+
+  > .removeChild()
+
+- #### filter
+
+  > forEach에서 function을 실행하는것과 같이 각각의 item과 같이 실행이 된다.
+  >
+  > 'array'을 하나 만든다.
+  >
+  > filter는 array의 모든 아이템을 통해 함수를 실행하고, 그리고 true인 아이템들만 가지고 새로운 array를 만들고
+
+- #### .parseInt()
+
+  > string을 숫자로바꿈
--- vs-study/public/todo.js
+++ vs-study/public/todo.js
@@ -5,8 +5,64 @@ const toDoForm = document.querySelector(".js-toDoForm"),
 // 로컬스토리지에서 할 일을 알면 그것들은 좀 이상한 걸 하면서 추가해줘야한다.
 const TODOS_LS = 'toDos';
 
+// array로 저장해야하기때문에
+// 해야할 일을 생성했을 때 toDOs  array에 추가되도록한다.
+let toDos = [];
+
+// li에 없는 id인 toDOs를 체크하고싶을때 = 지우고 싶은것
+// function filterFn(toDo) {
+//     // true 인 것들인 toDos인것만 출력
+//     return toDo.id === 1
+// }
+function deleteToDo(event) {
+    // console.log(event.target.parentNode);
+    const btn = event.target;
+    const li = btn.parentNode;
+    toDoList.removeChild(li);
+    // filterFn을 여기다가 넣으면 이것은 array 안에 있는
+    // 모든 toDos를 통한다.
+
+    const clseanToDos = toDos.filter(function (toDo) {
+        // toDo의 id가 숫자
+        // li의 id는 string
+        return toDo.id !== parseInt(li.id);
+    });
+    // cleanToDos와 filter가 하는것은 filterFn이 체크가 된 
+    // 아이템들의 array를 주는 것.
+    console.log(clseanToDos);
+    toDos = clseanToDos;
+    saveToDos(); // toDos를 저장하는 기능
+}
+// toDos를 가져와서 로컬에 저장하는 함수
+function saveToDos() {
+    localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
+}
+
 function paintToDo(text) {
     // console.log(text);
+    // HTML 만들기
+    const li = document.createElement("li");
+    const delBtn = document.createElement("button");
+    const span = document.createElement("span");
+    const newId = toDos.length + 1;
+    delBtn.innerText = "X";
+    delBtn.addEventListener("click", deleteToDo);
+
+    span.innerText = text;
+    li.appendChild(span);
+    li.appendChild(delBtn);
+    toDoList.appendChild(li);
+    // li에 id를 추가(원하는 li를 삭제하기위해)
+    li.id = newId;
+    // 'toDos' array 안에 'toDoObj'를 넣게 됨
+    const toDoObj = {
+        text: text,
+        id: newId
+    };
+    toDos.push(toDoObj);
+    saveToDos();
+
+    // todo를 이렇게 저장하는 이유는, local storage에도 투두를 저장해둬야하기 때문.
 }
 // 내가 하고 싶은건, 누군가 엔터를 눌렀을 떄 
 function handleSubmit(event) {
@@ -16,9 +72,16 @@ function handleSubmit(event) {
     toDoInput.value = "";
 }
 function loadToDos() {
-    const toDos = localStorage.getItem(TODOS_LS);
-    if (toDos !== null) {
-
+    const loadedToDos = localStorage.getItem(TODOS_LS);
+    if (loadedToDos !== null) {
+        const parsedToDos = JSON.parse(loadedToDos);
+        parsedToDos.forEach(function (toDo) {
+            // console.log(toDo.text);
+            // toDos를 가져온뒤, 이라인에서 parse 
+            // 즉 가져온 것을 자바스크립트 object로 변환해 줄것이고,
+            // 각각에 대해서 paintToDo라는 function이 실행됨.
+            paintToDo(toDo.text);
+        });
     }
 }
 // todo를 생성해주기.
