5c83dcab3f4344810daf8ae6b29cda810e23b7a0
yang2momo <59380943+yang2momo@users.noreply.github.com>
2020-02-18 16:47:00 +0900

    7장 라이프사이클
    p182

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,22 +1,33 @@
 import React, { Component } from 'react';
-import IterationSample from './IterationSample';
+import LifeCycleSample from './LifeCycleSample';
 
-// class App extends Component {
-//   render() {
-//     return (
-//       <div>
-//         <ScrollBox ref={(ref) => this.ScrollBox = ref} />
-//         <button onClick={() => this.ScrollBox.scrollToBottom()}>
-//           맨 밑으로
-//         </button>
-//       </div>
-//     );
-//   }
-// }
-const App = () => {
-  return <IterationSample />;
-
-
+function getRandomColor() {
+  return '#' + Math.floor(Math.random() * 16777215).toString(16);
 }
+
+class App extends Component {
+  state = {
+    color: '#000000'
+  }
+  handleClick = () => {
+    this.setState({
+      color: getRandomColor()
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.handleClick}>랜덤색상</button>
+        <LifeCycleSample color={this.state.color} />
+      </div>
+    );
+  }
+}
+// const App = () => {
+//   return <LifeCycleSample />;
+
+
+// }
 
 export default App;
